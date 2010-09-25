77ca74f0b572f6d6b8187c2540b72e5dc81e09ce
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-09-24 23:50:25 -0600

    move from one message handler per active channel to a single handler that does routing.  This means we only parse a message once, and msg dispatching is log(n) as opposed to exponential.  Also some restrictions are lifted, and others are made explicit (throw rather than just break).  I'm hoping this is a Good Change.

--- src/jschannel.js
+++ src/jschannel.js
@@ -41,7 +41,104 @@
     // There is one current transaction counter id per page, and it's shared between
     // channel instances.  That means of all messages posted from a single javascript
     // evaluation context, we'll never have two with the same id.
-    var curTranId = Math.floor(Math.random()*1000001);
+    var s_curTranId = Math.floor(Math.random()*1000001);
+
+    // no two bound channels in the same javascript evaluation context may have the same origin & scope.
+    // futher if two bound channels have the same scope, they may not have *overlapping* origins
+    // (either one or both support '*').  This restriction allows a single onMessage handler to efficiently
+    // route messages based on origin and scope.  The s_boundChans maps origins to scopes, to message
+    // handlers.  Request and Notification messages are routed using this table.
+    // Finally, channels are inserted into this table when built, and removed when destroyed.
+    var s_boundChans = { };
+
+    // add a channel to s_boundChans, throwing if a dup exists
+    function s_addBoundChan(origin, scope, handler) {
+        // does she exist?
+        var exists = false;
+        if (origin === '*') {
+            // we must check all other origins, sadly.
+            for (var k in s_boundChans) {
+                if (!s_boundChans.hasOwnProperty(k)) continue;
+                if (k === '*') continue;
+                if (typeof s_boundChans[k][scope] === 'object') {
+                    exists = true;
+                }
+            }
+        } else {
+            // we must check only '*'
+            if ((s_boundChans['*'] && s_boundChans['*'][scope]) ||
+                (s_boundChans[origin] && s_boundChans[origin][scope]))
+            {
+                exists = true;
+            }
+        }
+        if (exists) throw "A channel already exists which overlaps with origin '"+ origin +"' and has scope '"+scope+"'"; 
+
+        if (typeof s_boundChans[origin] != 'object') s_boundChans[origin] = { };
+        s_boundChans[origin][scope] = handler;
+    }
+
+    function s_removeBoundChan(origin, scope) {
+        delete s_boundChans[origin][scope];
+        // possibly leave a empty object around.  whatevs.
+    }
+
+    // No two outstanding outbound messages may have the same id, period.  Given that, a single table
+    // mapping "transaction ids" to message handlers, allows efficient routing of Callback, Error, and
+    // Response messages.  Entries are added to this table when requests are sent, and removed when
+    // responses are received.
+    var s_transIds = { };
+
+    // class singleton onMessage handler
+    // this function is registered once and all incoming messages route through here.  This
+    // arrangement allows certain efficiencies, message data is only parsed once and dispatch
+    // is more efficient, especially for large numbers of simultaneous channels.
+    var s_onMessage = function(e) {
+        var m = JSON.parse(e.data);
+        if (typeof m !== 'object') return;
+
+        var o = e.origin;
+        var s = null;
+        var i = null;
+        var meth = null;
+        
+        if (typeof m.method === 'string') {
+            var ar = m.method.split('::');
+            if (ar.length == 2) {
+                s = ar[0];
+                meth = ar[1];
+            } else {
+                meth = m.method;
+            }
+        }
+
+        if (typeof m.id !== 'undefined') i = m.id;
+
+        // o is message origin
+        // m is parsed message
+        // s is message scope
+        // i is message id (or null)
+        // meth is unscoped method name
+        // ^^ based on these factors we can route the message
+
+        // if it has a method it's either a notification or a request,
+        // route using s_boundChans
+        if (typeof meth === 'string') {
+            if (s_boundChans[o] && s_boundChans[o][s]) {
+                s_boundChans[origin][scope](o, meth, m);
+            } else if (s_boundChans['*'] && s_boundChans['*'][s]) {
+                s_boundChans['*'][s](o, meth, m);
+            }
+        }
+        // otherwise it must have an id (or be poorly formed
+        else if (typeof i != 'undefined') {
+            if (s_transIds[i]) s_transIds[i](o, meth, m);
+        }
+    };
+
+    // Setup postMessage event listeners
+    if (window.addEventListener) window.addEventListener('message', s_onMessage, false);
+    else if(window.attachEvent) window.attachEvent('onmessage', s_onMessage);
 
     /* a messaging channel is constructed from a window and an origin.
      * the channel will assert that all messages received over the
@@ -87,7 +184,9 @@
 
             /* browser capabilities check */
             if (!window.postMessage) throw("jschannel cannot run this browser, no postMessage");
-            if (!window.JSON || !window.JSON.stringify || ! window.JSON.parse) throw("jschannel cannot run this browser, no JSON parsing/serialization");
+            if (!window.JSON || !window.JSON.stringify || ! window.JSON.parse) {
+                throw("jschannel cannot run this browser, no JSON parsing/serialization");
+            }
 
             /* basic argument validation */
             if (typeof cfg != 'object') throw("Channel build invoked without a proper object argument");
@@ -96,7 +195,7 @@
 
             /* we'd have to do a little more work to be able to run multiple channels that intercommunicate the same
              * window...  Not sure if we care to support that */
-            if (window === cfg.window) throw("target window is same as present window -- communication within the same window not yet supported");   
+            if (window === cfg.window) throw("target window is same as present window -- not allowed");
 
             // let's require that the client specify an origin.  if we just assume '*' we'll be
             // propagating unsafe practices.  that would be lame.
@@ -185,36 +284,7 @@
                 };
             }
 
-            var onMessage = function(e) {
-                var handled = false;
-                debug("got    message: " + e.data);
-                // validate event origin
-                if (cfg.origin !== '*' && cfg.origin !== e.origin) {
-                    debug("dropping message, origin mismatch! '" + cfg.origin + "' !== '" + e.origin + "'");
-                    return;
-                }
-
-                // messages must be objects
-                var m = JSON.parse(e.data);
-                
-                if (typeof m !== 'object') return;
-
-                // first, descope method if it needs it
-                var unscopedMethod = m.method;
-
-                if (m.method && cfg.scope) {
-                    var ar = m.method.split('::');
-                    if (ar.length != 2) {
-                        debug("dropping message: has unscoped method name, I expect scoping to '" + cfg.scope + "'");
-                        return;
-                    }
-                    if (ar[0] !== cfg.scope) {
-                        debug("dropping message: out of scope: '" + ar[0] + "' !== '" + cfg.scope + "'");
-                        return;
-                    }
-                    unscopedMethod = ar[1];
-                }
-
+            var onMessage = function(origin, method, m) {
                 // if an observer was specified at allocation time, invoke it
                 if (typeof cfg.gotMessageObserver === 'function') {
                     // pass observer a clone of the object so that our
@@ -222,19 +292,16 @@
                     // This is not particularly efficient, but then we expect
                     // that message observers are primarily for debugging anyway.
                     try {
-                        cfg.gotMessageObserver(e.origin, JSON.parse(JSON.stringify(m)));
+                        cfg.gotMessageObserver(origin, m);
                     } catch (e) {
                         debug("gotMessageObserver() raised an exception: " + e.toString());
                     }
                 }
 
-                m.method = unscopedMethod;
-
-
                 // now, what type of message is this?
-                if (m.id && m.method) {
+                if (m.id && method) {
                     // a request!  do we have a registered handler for this request?
-                    if (regTbl[m.method]) {
+                    if (regTbl[method]) {
                         var trans = createTransaction(m.id, m.callbacks ? m.callbacks : [ ]);
                         inTbl[m.id] = { };
                         try {
@@ -258,7 +325,7 @@
                                     })();
                                 }
                             }
-                            var resp = regTbl[m.method](trans, m.params);
+                            var resp = regTbl[method](trans, m.params);
                             if (!trans.delayReturn() && !trans.completed()) trans.complete(resp);
                         } catch(e) {
                             // automagic handling of exceptions:
@@ -294,14 +361,12 @@
 
                             trans.error(error,message);
                         }
-                        handled = true;
                     }
                 } else if (m.id && m.callback) {
                     if (!outTbl[m.id] ||!outTbl[m.id].callbacks || !outTbl[m.id].callbacks[m.callback])
                     {
                         debug("ignoring invalid callback, id:"+m.id+ " (" + m.callback +")");
                     } else {
-                        handled = true;
                         // XXX: what if client code raises an exception here?
                         outTbl[m.id].callbacks[m.callback](m.params);
                     }
@@ -309,8 +374,6 @@
                     if (!outTbl[m.id]) {
                         debug("ignoring invalid response: " + m.id);
                     } else {
-                        handled = true;
-                        
                         // XXX: what if client code raises an exception here?
                         if (m.error) {
                             outTbl[m.id].error(m.error, m.message);
@@ -318,26 +381,22 @@
                             outTbl[m.id].success(m.result);
                         }
                         delete outTbl[m.id];
+                        delete s_transIds[m.id];
                     }
-                } else if (m.method) {
+                } else if (method) {
                     // tis a notification.
-                    if (regTbl[m.method]) {
+                    if (regTbl[method]) {
                         // yep, there's a handler for that.
                         // transaction is null for notifications.
-                        regTbl[m.method](null, m.params);
+                        regTbl[method](null, m.params);
                         // if the client throws, we'll just let it bubble out
                         // what can we do?  Also, here we'll ignore return values
-                        handled = true;
                     }
                 }
-
-                if (handled) {
-                    // we got it, hands off.
-                    try { e.stopPropogation(); } catch(excp) { }
-                } else {
-                    debug("Ignoring event: " + e.data);
-                }
             }
+
+            // now register our bound channel for msg routing
+            s_addBoundChan(cfg.origin, ((typeof cfg.scope === 'string') ? cfg.scope : ''), onMessage);
 
             // scope method names based on cfg.scope specified when the Channel was instantiated 
             var scopeMethod = function(m) {
@@ -395,10 +454,6 @@
                 if (typeof cfg.onReady === 'function') cfg.onReady(obj);
             };
 
-            // Setup postMessage event listeners
-            if (window.addEventListener) window.addEventListener('message', onMessage, false);
-            else if(window.attachEvent) window.attachEvent('onmessage', onMessage);
-
             var obj = {
                 // tries to unbind a bound message handler.  returns false if not possible
                 unbind: function (method) {
@@ -443,14 +498,15 @@
                     pruneFunctions("", m.params);
 
                     // build a 'request' message and send it
-                    var msg = { id: curTranId, method: scopeMethod(m.method), params: m.params };
+                    var msg = { id: s_curTranId, method: scopeMethod(m.method), params: m.params };
                     if (callbackNames.length) msg.callbacks = callbackNames;
 
                     // insert into the transaction table
-                    outTbl[curTranId] = { callbacks: callbacks, error: m.error, success: m.success };
+                    outTbl[s_curTranId] = { callbacks: callbacks, error: m.error, success: m.success };
+                    s_transIds[s_curTranId] = onMessage;
 
                     // increment current id
-                    curTranId++;
+                    s_curTranId++;
 
                     postMessage(msg);
                 },
@@ -462,6 +518,7 @@
                     postMessage({ method: scopeMethod(m.method), params: m.params });
                 },
                 destroy: function () {
+                    s_removeBoundChan(cfg.origin, ((typeof cfg.scope === 'string') ? cfg.scope : ''));
                     if (window.removeEventListener) window.removeEventListener('message', onMessage, false);
                     else if(window.detachEvent) window.detachEvent('onmessage', onMessage);
                     ready = false;
